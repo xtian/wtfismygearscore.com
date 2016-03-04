27d833687a5ba95c7d7d7f92054e6be41aab1964
Christian Wesselhoeft <hi@xtian.us>
2016-03-04 00:18:01 -0700

    Add Google Analytics snippet

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -5,5 +5,6 @@ RSpec.feature 'Homepage' do
     visit '/'
 
     expect(page).to have_title('WTF is My Gear Score?')
+    expect(page).to have_css('script[src="https://www.google-analytics.com/analytics.js"]', visible: false, count: 1)
   end
 end
