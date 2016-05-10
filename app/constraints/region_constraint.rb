70dafeb4842947d14fe4a6ee361e22b968d09248
Christian Wesselhoeft <hi@xtian.us>
2016-05-10 13:16:52 -0600

    RegionConstraint: Handle uppercase regions

--- app/constraints/region_constraint.rb
+++ app/constraints/region_constraint.rb
@@ -3,7 +3,7 @@ class RegionConstraint
   REALM_REGIONS_SET = Set.new(VALID_REGIONS_WITH_REALM).freeze
 
   def matches?(request)
-    region = request.params[:region]
+    region = request.params[:region].downcase
 
     if request.params[:realm]
       REALM_REGIONS_SET.include? region
