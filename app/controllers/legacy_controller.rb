6cf0ba42a7bf89f2047f852bec2644e72333f9aa
Christian Wesselhoeft <hi@xtian.us>
2016-05-10 11:59:24 -0600

    Handle legacy redirects

---
+++ app/controllers/legacy_controller.rb
@@ -0,0 +1,27 @@
+class LegacyController < ApplicationController
+  def index
+    redirect_to characters_path(*legacy_params), status: :moved_permanently
+  end
+
+  def show
+    redirect_params = legacy_params.compact
+
+    if redirect_params.length == 3
+      redirect_to character_path(*redirect_params), status: :moved_permanently
+    else
+      not_found
+    end
+  end
+
+  private
+
+  def legacy_params
+    if params[:d]
+      params.values_at(:d, :r, :n)
+    elsif params[:r]
+      params.values_at(:r, :s, :n)
+    else
+      ['world']
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'redirect', to: 'home#redirect', as: :redirect
 
+  get 'gearscore.php', to: 'legacy#show'
+  get 'rankings.php', to: 'legacy#index'
+
   constraints RegionConstraint.new do
     get ':region/:realm/:name', to: 'characters#show', as: :character
     get ':region(/:realm)', to: 'characters#index', as: :characters
