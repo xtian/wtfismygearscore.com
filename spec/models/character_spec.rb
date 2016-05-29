28b7bcbe9f9775b8a8990ad99d8e56595bc0ad87
Christian Wesselhoeft <hi@xtian.us>
2016-05-29 09:13:21 -0600

    CharacterSpec: Condense shoulda-matchers specs

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -4,21 +4,13 @@ RSpec.describe Character do
   it { should define_enum_for(:class_name).with(CLASSES) }
   it { should define_enum_for(:region).with(VALID_REGIONS_WITH_REALM) }
 
-  it { should validate_presence_of(:avg_ilvl) }
-  it { should validate_presence_of(:class_name) }
-  it { should validate_presence_of(:level) }
-  it { should validate_presence_of(:max_ilvl) }
-  it { should validate_presence_of(:min_ilvl) }
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:realm) }
-  it { should validate_presence_of(:region) }
-  it { should validate_presence_of(:score) }
+  %i(avg_ilvl class_name level max_ilvl min_ilvl name realm region score).each do |field|
+    it { should validate_presence_of(field) }
+  end
 
-  it { should validate_numericality_of(:avg_ilvl).only_integer.is_greater_than(0) }
-  it { should validate_numericality_of(:level).only_integer.is_greater_than(0) }
-  it { should validate_numericality_of(:max_ilvl).only_integer.is_greater_than(0) }
-  it { should validate_numericality_of(:min_ilvl).only_integer.is_greater_than(0) }
-  it { should validate_numericality_of(:score).only_integer.is_greater_than(0) }
+  %i(avg_ilvl level max_ilvl min_ilvl score).each do |field|
+    it { should validate_numericality_of(field).only_integer.is_greater_than(0) }
+  end
 
   it { should have_db_index([:name, :realm, :region]).unique }
   it { should have_db_index([:realm, :region, :score]) }
