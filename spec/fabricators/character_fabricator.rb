93b106916f56dad840ea64db390bb947171ea0e4
Christian Wesselhoeft <hi@xtian.us>
2016-05-29 18:21:39 -0600

    CharacterFabricator: Generate random #guild_name

--- spec/fabricators/character_fabricator.rb
+++ spec/fabricators/character_fabricator.rb
@@ -1,7 +1,6 @@
 Fabricator(:character) do
   avg_ilvl { rand(700) + 1 }
   class_name { CLASSES.sample }
-  guild_name 'The Gentlemens Club'
   level { rand(100) + 1 }
   max_ilvl { rand(700) + 1 }
   min_ilvl { rand(700) + 1 }
@@ -11,5 +10,9 @@ Fabricator(:character) do
 
   name do
     sequence(:name) { |i| "Character#{i}" }
+  end
+
+  guild_name do
+    sequence(:guild_name) { |i| "Guild#{i}" }
   end
 end
