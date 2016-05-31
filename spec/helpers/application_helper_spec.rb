479312f9744ad7a46b249431cc358cf84983d52c
Christian Wesselhoeft <hi@xtian.us>
2016-05-30 20:01:37 -0600

    ApplicationHelper: Add subtitle support to #page_title

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper do
+  describe '#page_title' do
+    it 'joins page title and site name' do
+      title = helper.page_title(
+        page_title: 'page title',
+        site_name: 'site name',
+        subtitle: 'subtitle'
+      )
+
+      expect(title).to eq('page title — site name')
+    end
+
+    it 'includes subtitle if no page title is given' do
+      title = helper.page_title(
+        page_title: nil,
+        site_name: 'site name',
+        subtitle: 'subtitle'
+      )
+
+      expect(title).to eq('site name — subtitle')
+    end
+  end
+end
