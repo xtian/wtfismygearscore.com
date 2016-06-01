0afa69aa0ba5bf0109f6897685e23d31fcdbb3b3
Christian Wesselhoeft <hi@xtian.us>
2016-06-01 10:25:18 -0600

    Simplify character link helper and move to HomeHelper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,4 +1,9 @@
 module HomeHelper
+  def character_link(character)
+    css_class_name = character.class_name.parameterize.underscore.camelize(:lower)
+    link_to(character.name, character, class: "CharacterLink--#{css_class_name}")
+  end
+
   def region_options
     VALID_REGIONS_WITH_REALM.map { |r| [r.upcase, r] }
   end
