d381320c75942c38cddb26a1bb71f726268c0bf8
Christian Wesselhoeft <hi@xtian.us>
2016-06-07 15:32:22 -0600

    MedianGearscoreUpdaterJob: Fix to create non-existent medians

--- app/jobs/median_gearscore_updater_job.rb
+++ app/jobs/median_gearscore_updater_job.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 class MedianGearscoreUpdaterJob < ApplicationJob
   def perform
-    MedianGearscore.find_each(&:calculate)
+    Character.group(:level).count.keys.each do |level|
+      MedianGearscore.find_or_initialize_by(level: level).calculate
+    end
   ensure
     self.class.set(wait_until: Date.tomorrow.midnight).perform_later
   end
