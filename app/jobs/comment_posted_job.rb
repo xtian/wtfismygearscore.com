cb3829c676b6bd82fff3729af1afad4d0d69f2dd
Christian Wesselhoeft <hi@xtian.us>
2016-06-10 10:22:28 -0600

    CommentPostedJob: Handle nil Rakismet.key

--- app/jobs/comment_posted_job.rb
+++ app/jobs/comment_posted_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class CommentPostedJob < ApplicationJob
   def perform(comment)
-    return comment.destroy! if comment.spam?
+    return comment.destroy! if Rakismet.key && comment.spam?
     RecentComment.refresh
   end
 end
