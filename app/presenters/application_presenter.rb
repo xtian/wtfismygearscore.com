7ff6fd23d7064ce15d400cf73e0db165e1274cea
Christian Wesselhoeft <hi@xtian.us>
2016-06-16 12:12:58 -0600

    Extract ApplicationPresenter::CollectionProxy into CollectionPresenter

--- app/presenters/application_presenter.rb
+++ app/presenters/application_presenter.rb
@@ -1,26 +1,9 @@
 # frozen_string_literal: true
 class ApplicationPresenter < SimpleDelegator
-  def self.present_collection(collection)
-    CollectionProxy.new(collection, self)
+  def self.present_collection(collection, presenter = CollectionPresenter)
+    presenter.new(collection, self)
   end
 
   alias object __getobj__
   private :object
-
-  class CollectionProxy < SimpleDelegator
-    include Enumerable
-
-    def initialize(collection, presenter)
-      @presenter = presenter
-      super collection
-    end
-
-    def each
-      __getobj__.each { |item| yield @presenter.new(item) }
-    end
-
-    def to_ary
-      self.class.new(__getobj__.to_ary, @presenter)
-    end
-  end
 end
