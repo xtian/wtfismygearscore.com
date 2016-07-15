f53651a5316d3df8e4855348e966665560e49f83
Christian Wesselhoeft <hi@xtian.us>
2016-07-14 19:40:19 -0600

    CommentPostedJob: Add documentation

--- app/jobs/comment_posted_job.rb
+++ app/jobs/comment_posted_job.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
+
+# Destroys {Comment} if it registers as spam. Otherwise updates
+# {RecentComment} materialized view.
 class CommentPostedJob < ApplicationJob
+  # @param comment [Comment] posted comment
+  # @param referrer [String] referrer for comment post request
+  # @param user_agent [String] user agent for poster's browser
+  # @return void
   def perform(comment, referrer, user_agent)
     @comment = comment
     @referrer = referrer
