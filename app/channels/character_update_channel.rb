0fd5a9ba9cde8c68b0f4ee982346759fdc9ae13a
Christian Wesselhoeft <hi@xtian.us>
2016-07-14 19:22:10 -0600

    CharacterUpdateChannel: Add documentation

--- app/channels/character_update_channel.rb
+++ app/channels/character_update_channel.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
+
+# Sets up connection to character page client code.
+# Updates consist of HTML content which is sent in response to back-end
+# model changes.
 class CharacterUpdateChannel < ApplicationCable::Channel
+  # @option data [String] 'id' id of {Character} to stream updates for
+  # @option data [String] 'timestamp' timestamp of when client character data
+  #   was last updated
+  # @return void
   def follow(data)
     stop_all_streams
     raise "Invalid data: #{data}" unless valid_data?(data)
@@ -7,12 +15,15 @@ class CharacterUpdateChannel < ApplicationCable::Channel
     character = Character.find(data['id'])
     stream_from "characters:#{character.to_param}:armory_updates"
 
+    # Try to trigger broadcast immediately as character may have been updated
+    # in time between initial request and ActionCable connection.
     CharacterUpdateBroadcaster.call(character, data['timestamp'])
 
   rescue ActiveRecord::RecordNotFound
     logger.warn "CharacterUpdateChannel: No Character with found with id: #{data['id']}"
   end
 
+  # @return void
   def unfollow
     stop_all_streams
   end
@@ -20,6 +31,6 @@ class CharacterUpdateChannel < ApplicationCable::Channel
   private
 
   def valid_data?(data)
-    data['id'] && data['timestamp']
+    data['id'].present? && data['timestamp'].present?
   end
 end
