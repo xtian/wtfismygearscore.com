dff4184f7530c150b50ca371d54bfa801f621adc
Christian Wesselhoeft <hi@xtian.us>
2016-07-15 11:01:03 -0600

    WarningFilter: Add documentation

--- lib/warning_filter.rb
+++ lib/warning_filter.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 require 'delegate'
 
-# Filters Ruby warnings from gems out of $stderr when $VERBOSE is enabled
-
+# Filters Ruby warnings from gems out of `$stderr` when `$VERBOSE` is enabled
 class WarningFilter < DelegateClass(IO)
+  # @return [String, nil]
   def write(line)
     super unless line.strip.empty? || line =~ %r{^.+\/gems\/.+: warning:}
   end
