14a04e01eb7641d7bf1b87ec95042b4ac07677dc
Christian Wesselhoeft <hi@xtian.us>
2016-07-14 19:43:38 -0600

    MedianGearscoreUpdaterJob: Add documentation

--- app/jobs/median_gearscore_updater_job.rb
+++ app/jobs/median_gearscore_updater_job.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
+
 class MedianGearscoreUpdaterJob < ApplicationJob
+  # Triggers a median gearscore calculation for each distinct character level
+  # in the DB. Afterwards, re-enqueues self for following midnight.
+  # @return void
   def perform
     Character.group(:level).count.keys.each do |level|
       MedianGearscore.find_or_initialize_by(level: level).calculate
