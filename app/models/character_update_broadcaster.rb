a7fcc22dc34a36d15305fcde97933b503f9c954d
Christian Wesselhoeft <hi@xtian.us>
2016-07-15 09:58:52 -0600

    CharacterUpdateBroadcaster: Add documentation

--- app/models/character_update_broadcaster.rb
+++ app/models/character_update_broadcaster.rb
@@ -1,14 +1,25 @@
 # frozen_string_literal: true
+
+# Encapsulates logic for broadcasting HTML character updates over ActionCable
 class CharacterUpdateBroadcaster
+  # @param character [Character]
+  # @param updated_at [String, DateTime]
   def initialize(character, updated_at)
     @caller_updated_at = updated_at
     @character = character
   end
 
+  # Convenience method to avoid object initialization
+  # @param character [Character]
+  # @param updated_at [String, DateTime]
+  # @return void
   def self.call(character, updated_at)
     new(character, updated_at).call
   end
 
+  # Broadcasts update to the relevant {CharacterUpdateChannel} if given
+  # {Character} was updated more recently than given timestamp
+  # @return void
   def call
     return unless character.updated_at > caller_updated_at
     ActionCable.server.broadcast channel_name, html: partial, id: character.to_param
