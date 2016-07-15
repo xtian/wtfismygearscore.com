d0680c97902db0a43954793f5804e40526390a8c
Christian Wesselhoeft <hi@xtian.us>
2016-07-15 10:56:19 -0600

    RecentCommentPresenter: Add documentation

--- app/presenters/recent_comment_presenter.rb
+++ app/presenters/recent_comment_presenter.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 class RecentCommentPresenter < CommentPresenter
+  # @return [Array<String>]
   def character_info
     [character_region, character_realm, character_name]
   end
 
+  # @return [String]
   def character_link_class
     "CharacterLink--#{class_name}"
   end
 
   private
+
+  # RecentComment doesn't have access to the correct character enum field
+  # values so these methods accomodate for that.
 
   def class_name
     class_name = CLASSES[character_class]
