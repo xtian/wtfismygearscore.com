bda3a09c053e91bc4f4d7ff8344a59d72d06c7cc
Christian Wesselhoeft <hi@xtian.us>
2016-07-15 10:55:38 -0600

    RecentComment: Add documentation

--- app/models/recent_comment.rb
+++ app/models/recent_comment.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
+
+# Materialized view which caches a limited number of recent comments for
+# display so that an index does not have to be maintained across the entire
+# comments table
 class RecentComment < ApplicationRecord
+  # Refreshes materialized view
+  # @return void
   def self.refresh
     Scenic.database.refresh_materialized_view(table_name, concurrently: true)
   end
