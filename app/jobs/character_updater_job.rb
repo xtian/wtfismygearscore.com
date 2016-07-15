6251b2ed8e97b0f4bff9e26a82e467cd5769387b
Christian Wesselhoeft <hi@xtian.us>
2016-07-14 22:31:29 -0600

    CharacterUpdaterJob: Add documentation

--- app/jobs/character_updater_job.rb
+++ app/jobs/character_updater_job.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
+
+# Wraps {CharacterUpdater} and {CharacterUpdateBroadcaster} in a background job
 class CharacterUpdaterJob < ApplicationJob
+  # @param character [Character] character to check for updates
+  # @return void
   def perform(character)
     broadcast(update(character), character.updated_at)
   end
