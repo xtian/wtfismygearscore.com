40eb8315d5eead2646c0b3616f80e384b46bc4a9
Christian Wesselhoeft <hi@xtian.us>
2016-07-14 23:10:06 -0600

    CharacterPresenter: Add documentation

--- app/presenters/character_presenter.rb
+++ app/presenters/character_presenter.rb
@@ -1,27 +1,34 @@
 # frozen_string_literal: true
 class CharacterPresenter < ApplicationPresenter
+  # @return [String] URL to character's Armory page
   def armory_url
+    # If trailing slash is left off, Armory will return 404.
     "https://#{region}.battle.net/wow/en/character/#{realm}/#{name}/"
   end
 
+  # @return [String] character's class converted to CSS class identifier
   def css_class_name
     class_name.parameterize.underscore.camelize(:lower)
   end
 
+  # @return [Array<CommentPresenter>]
   def comments
     CommentPresenter.present_collection(super)
   end
 
+  # @return [String]
   def info
     "#{name.humanize}, #{level} #{class_name.titleize}"
   end
 
+  # @return [Fixnum, Float] median difference with trailing zero trimmed
   def median_difference
     float = super
     int = float.to_i
     int == float ? int : float
   end
 
+  # @return [String]
   def rating
     median_difference >= 0 ? 'win' : 'fail'
   end
