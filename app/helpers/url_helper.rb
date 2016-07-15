86bb73ca7566e8eb85c912d8f83448ee54ed3cf6
Christian Wesselhoeft <hi@xtian.us>
2016-07-14 22:41:07 -0600

    UrlHelper: Add documentation

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 module UrlHelper
+  # @overload character_path(character)
+  #   @param character [Character]
+  # @overload character_path(region, realm, name)
+  #   @param region [String]
+  #   @param realm [String]
+  #   @param name [String]
+  # @return [String] path to character page
   def character_path(*args)
     return super if args.length == 3
 
@@ -7,6 +14,13 @@ module UrlHelper
     super(character.region, character.realm, character.name)
   end
 
+  # @overload comments_path(character)
+  #   @param character [Character]
+  # @overload comments_path(region, realm, name)
+  #   @param region [String]
+  #   @param realm [String]
+  #   @param name [String]
+  # @return [String] path to comments route
   def comments_path(*args)
     return super if args.length == 3
     "#{character_path(args.first)}/comments"
