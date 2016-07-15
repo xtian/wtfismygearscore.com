787ecfaa7b8b285954f73f51e0b0ba74889d2c38
Christian Wesselhoeft <hi@xtian.us>
2016-07-15 10:38:03 -0600

    RankedCharactersPresenter: Add documentation

--- app/presenters/ranked_characters_presenter.rb
+++ app/presenters/ranked_characters_presenter.rb
@@ -1,26 +1,33 @@
 # frozen_string_literal: true
 class RankedCharactersPresenter < CollectionPresenter
+  # @param characters [Enumerable<Character>]
+  # @param query [RankingQuery]
   def initialize(characters, query)
     @query = query
     super(characters, CharacterPresenter)
   end
 
+  # @return [String]
   def extra_column_name
     query.realm ? 'Guild' : 'Realm'
   end
 
+  # @return [Boolean]
   def first_page?
     !collection.first || collection.first.rank == 1
   end
 
+  # @return [Boolean]
   def last_page?
     collection.size < query.per_page
   end
 
+  # @return [Fixnum, nil]
   def next_cursor
     collection.last&.id
   end
 
+  # @return [Fixnum, nil]
   def prev_cursor
     collection.first&.id
   end
