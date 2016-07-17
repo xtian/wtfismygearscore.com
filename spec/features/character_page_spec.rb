d2e553b92b3d5c041a1609a20ac0b4b2b0d5bb41
Christian Wesselhoeft <hi@xtian.us>
2016-07-16 19:31:35 -0600

    CommentsPageSpec: Extract comments pagination into separate scenario

--- spec/features/character_page_spec.rb
+++ spec/features/character_page_spec.rb
@@ -28,31 +28,19 @@ RSpec.feature 'Character page' do
     expect(character_page.rating).to eq('win')
   end
 
-  scenario 'User posts a comment' do
+  scenario 'User visits another page of comments' do
     character = Fabricate(:character)
     Fabricate(:comment, body: 'hey', character: character, created_at: 2.weeks.ago)
     Fabricate(:comment, body: 'cool', character: character, created_at: 1.week.ago)
 
-    visit character_path(character, per_page: 2)
+    visit character_path(character, per_page: 1)
 
-    character_page.fill_comment_body 'hi'
-    character_page.fill_comment_name 'IdealPoster'
+    expect(character_page.comments_count).to eq(2)
+    expect(character_page.comments.length).to eq(1)
 
-    expect {
-      character_page.submit_comment
-    }.to have_enqueued_job(CommentPostedJob)
-
-    expect(character_page.notice_message).to eq(I18n.t('comment.posted_successfully'))
-
-    expect(character_page.comments_count).to eq(3)
-    expect(character_page.comments.length).to eq(2)
-
-    expect(character_page.comments[0].body).to eq('hi')
-    expect(character_page.comments[0].name).to eq('IdealPoster')
-
-    expect(character_page.comments[1].body).to eq('cool')
-    expect(character_page.comments[1].name).to eq('Anonymous')
-    expect(character_page.comments[1].posted_at).to eq(1.week.ago.to_date)
+    expect(character_page.comments[0].body).to eq('cool')
+    expect(character_page.comments[0].name).to eq('Anonymous')
+    expect(character_page.comments[0].posted_at).to eq(1.week.ago.to_date)
 
     expect(character_page.prev_page?).to eq(false)
     character_page.next_page
@@ -63,6 +51,22 @@ RSpec.feature 'Character page' do
     expect(character_page.next_page?).to eq(false)
     character_page.prev_page
 
+    expect(character_page.comments[0].body).to eq('cool')
+  end
+
+  scenario 'User posts a comment' do
+    visit character_path('us', 'shadowmoon', 'dargonaut')
+
+    character_page.fill_comment_body 'hi'
+    character_page.fill_comment_name 'IdealPoster'
+
+    expect {
+      character_page.submit_comment
+    }.to have_enqueued_job(CommentPostedJob)
+
+    expect(character_page.notice_message).to eq(I18n.t('comment.posted_successfully'))
+
     expect(character_page.comments[0].body).to eq('hi')
+    expect(character_page.comments[0].name).to eq('IdealPoster')
   end
 end
