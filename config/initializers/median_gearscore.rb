90a20b8634a5ff871fe4577ec8909ba514f37f31
Christian Wesselhoeft <hi@xtian.us>
2016-07-24 14:51:39 -0600

    Use Sidekiq event callback to enqueue first MedianGearscoreUpdaterJob

--- config/initializers/median_gearscore.rb
+++ config/initializers/median_gearscore.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
-Sidekiq.configure_client do
-  Rails.application.config.after_initialize do
+Sidekiq.configure_server do |config|
+  config.on(:startup) do
     MedianGearscoreUpdaterJob.perform_later unless Rails.env.test?
   end
 end
