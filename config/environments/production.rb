c9960673e462dabec128930495d067004333a281
Christian Wesselhoeft <hi@xtian.us>
2016-07-27 08:59:52 -0600

    Set production ActionCable origins with env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
 
   # Action Cable endpoint configuration
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = secrets.cable_origins
 
   # Don't mount Action Cable in the main server process.
   # config.action_cable.mount_path = nil
