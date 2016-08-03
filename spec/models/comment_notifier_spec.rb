db91dbc3e0a3c048f91183be4cbca95c7d55e3f6
Christian Wesselhoeft <hi@xtian.us>
2016-08-03 10:29:18 -0600

    CommentNotifier: Serialize poster IP address to string

--- spec/models/comment_notifier_spec.rb
+++ spec/models/comment_notifier_spec.rb
@@ -4,10 +4,11 @@ require 'rails_helper'
 RSpec.describe CommentNotifier do
   describe '.call' do
     it 'triggers a Slack webhook notification' do
-      comment = instance_double('Comment', poster_name: nil, poster_ip_address: '0.0.0.0', body: 'hi')
+      comment = instance_double('Comment', poster_name: nil, poster_ip_address: IPAddr.new('0.0.0.0'), body: 'hi')
       character = instance_double('Character', region: 'us', realm: 'Shadowmoon', name: 'Dargonaut')
 
       slack_request = stub_request(:post, Rails.application.secrets.slack_webhook_url)
+        .with(body: /0\.0\.0\.0/)
 
       described_class.call(comment, character)
 
