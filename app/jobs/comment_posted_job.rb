1eab51714f44d82b16f1aa2f6d506ef1330ef232
Christian Wesselhoeft <hi@xtian.us>
2016-08-03 10:13:09 -0600

    Remove Akismet initializer

--- app/jobs/comment_posted_job.rb
+++ app/jobs/comment_posted_job.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'akismet'
 
 # Destroys {Comment} if it registers as spam. Otherwise updates
 # {RecentComment} materialized view.
@@ -21,6 +22,12 @@ class CommentPostedJob < ApplicationJob
   private
 
   def spam?
-    AKISMET.spam?(@comment, referrer: @referrer, user_agent: @user_agent)
+    key = Rails.application.secrets.akismet_key
+    url = Rails.application.secrets.akismet_url
+
+    return false unless key && url
+
+    akismet = Akismet.new(key: key, url: url, is_test: Rails.env.in?(%w(development test)))
+    akismet.spam?(@comment, referrer: @referrer, user_agent: @user_agent)
   end
 end
