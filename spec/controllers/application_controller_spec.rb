2441c9c8e4d3fd36419673f8866b637f6c789873
Christian Wesselhoeft <hi@xtian.us>
2016-12-29 10:31:04 -0500

    ApplicationController: Handle InvalidAuthenticityToken exceptions

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -6,11 +6,20 @@ RSpec.describe ApplicationController do
     def index
       render html: 'foo'
     end
+
+    def create
+      raise ActionController::InvalidAuthenticityToken
+    end
   end
 
   it 'assigns UUID cookie' do
     get :index
 
     expect(cookies.signed[:uuid]).to be_present
+  end
+
+  it 'handles authenticity token exceptions' do
+    post :create
+    expect(response.status).to eq(400)
   end
 end
