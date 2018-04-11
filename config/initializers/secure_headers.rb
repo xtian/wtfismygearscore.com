d250d926a1bc84625891a520dfe8eea329d2b8fa
Christian Wesselhoeft <hi@xtian.us>
2018-04-11 17:21:00 -0600

    initializers/secure_headers.rb: Allow inline styles
    Should fix https://github.com/webcompat/web-bugs/issues/8870

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -16,8 +16,7 @@ SecureHeaders::Configuration.default do |config|
     default_src: %w['none'],
     connect_src: %w['self'] << (Rails.env.production? ? 'wss:' : 'ws:'),
     img_src: %w['self' data: www.google-analytics.com],
-    # Hashes are to allow inline error page styles
-    style_src: %w['self' 'sha256-vRzNr8OXAI2PQ1gJpDovs4Q1XX0wpMl7zKd9MQyJpTw='],
+    style_src: %w['self' 'unsafe-inline'],
     script_src: %w['self' www.google-analytics.com 'sha256-5/w5wEj5C2EFROmL8m0pVUE+CraUgYqRYT1b/0vrU70='],
     block_all_mixed_content: true,
     upgrade_insecure_requests: Rails.env.production?
