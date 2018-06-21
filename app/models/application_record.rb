8fa8ed7dd2f1aba6256d24a48bfd99339a7b3d16
Christian Wesselhoeft <hi@xtian.us>
2018-06-20 18:31:29 -0600

    Update Rails to 5.2.0

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,10 +2,4 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  # rubocop:disable Style/AndOr
-  def upsert!
-    upsert or raise ActiveRecord::RecordInvalid
-  end
-  # rubocop:enable Style/AndOr
 end
