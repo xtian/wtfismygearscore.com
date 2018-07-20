dbeeef92faa6a7e8cb9323eee3e89cec418b4147
Christian Wesselhoeft <hi@xtian.us>
2018-07-20 09:01:15 -0600

    Update gems

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,49 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930133403) do
+ActiveRecord::Schema.define(version: 2016_09_30_133403) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
   enable_extension "citext"
-
-  create_table "ar_internal_metadata", primary_key: "key", id: :string, force: :cascade do |t|
-    t.string   "value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  enable_extension "plpgsql"
 
   create_table "characters", force: :cascade do |t|
-    t.integer  "region",     null: false
-    t.citext   "realm",      null: false
-    t.citext   "name",       null: false
-    t.integer  "class_name", null: false
-    t.integer  "level",      null: false
-    t.integer  "score",      null: false
+    t.integer "region", null: false
+    t.citext "realm", null: false
+    t.citext "name", null: false
+    t.integer "class_name", null: false
+    t.integer "level", null: false
+    t.integer "score", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "avg_ilvl",   null: false
-    t.integer  "max_ilvl",   null: false
-    t.integer  "min_ilvl",   null: false
-    t.string   "guild_name"
-    t.integer  "faction",    null: false
-    t.index ["name", "realm", "region"], name: "index_characters_on_name_and_realm_and_region", unique: true, using: :btree
-    t.index ["score", "region", "realm", "name"], name: "index_characters_on_score_and_region_and_realm_and_name", order: { score: :desc }, using: :btree
+    t.integer "avg_ilvl", null: false
+    t.integer "max_ilvl", null: false
+    t.integer "min_ilvl", null: false
+    t.string "guild_name"
+    t.integer "faction", null: false
+    t.index ["name", "realm", "region"], name: "index_characters_on_name_and_realm_and_region", unique: true
+    t.index ["score", "region", "realm", "name"], name: "index_characters_on_score_and_region_and_realm_and_name", order: { score: :desc }
   end
 
   create_table "comments", force: :cascade do |t|
-    t.text     "body",              null: false
-    t.inet     "poster_ip_address", null: false
-    t.string   "poster_name"
-    t.integer  "character_id",      null: false
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.string   "referrer"
-    t.string   "user_agent"
-    t.index ["character_id", "created_at"], name: "index_comments_on_character_id_and_created_at", order: { created_at: :desc }, using: :btree
+    t.text "body", null: false
+    t.inet "poster_ip_address", null: false
+    t.string "poster_name"
+    t.integer "character_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "referrer"
+    t.string "user_agent"
+    t.index ["character_id", "created_at"], name: "index_comments_on_character_id_and_created_at", order: { created_at: :desc }
   end
 
-  create_table "median_gearscores", primary_key: "level", id: :integer, force: :cascade do |t|
+  create_table "median_gearscores", primary_key: "level", id: :serial, force: :cascade do |t|
     t.float "median_score", null: false
   end
 
@@ -64,7 +56,7 @@ ActiveRecord::Schema.define(version: 20160930133403) do
 
   add_foreign_key "comments", "characters", on_delete: :cascade
 
-  create_view :recent_comments, materialized: true,  sql_definition: <<-SQL
+  create_view "recent_comments", materialized: true,  sql_definition: <<-SQL
       SELECT comments.class_name AS character_class,
       comments.name AS character_name,
       comments.realm AS character_realm,
@@ -90,6 +82,6 @@ ActiveRecord::Schema.define(version: 20160930133403) do
     ORDER BY comments.created_at DESC;
   SQL
 
-  add_index "recent_comments", ["comment_id"], name: "index_recent_comments_on_comment_id", unique: true, using: :btree
+  add_index "recent_comments", ["comment_id"], name: "index_recent_comments_on_comment_id", unique: true
 
 end
