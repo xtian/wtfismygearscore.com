4bcfc59342fd6f01a944fabd8eb1409045164521
Christian Wesselhoeft <christian.wesselhoeft@gmail.com>
2018-09-12 09:21:40 -0600

    Update gems (#102)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,11 +3,9 @@
 require 'sidekiq'
 
 Sidekiq.default_worker_options = {
-  unique: :until_and_while_executing,
+  lock: :until_and_while_executing,
   unique_args: ->(args) { args.first.except('job_id') }
 }
-
-SidekiqUniqueJobs.config.unique_args_enabled = true
 
 Sidekiq.configure_server do |config|
   config.on(:startup) do
