6185dd5ca8d09d6f263930004ed2a887c49961a4
Christian Wesselhoeft <hi@xtian.us>
2018-09-12 07:47:38 -0600

    Fix Armory timeout

--- config/initializers/armory.rb
+++ config/initializers/armory.rb
@@ -3,6 +3,6 @@
 require 'armory'
 
 api_key = Rails.application.secrets.armory_api_key
-timeout = Rails.application.secrets.request_timeout.to_i - 2
+timeout = 12
 
 ARMORY = Armory.new(api_key, timeout)
