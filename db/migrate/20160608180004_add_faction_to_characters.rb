724ec5f0020493aad3428751ed9d09cd8ec07446
Christian Wesselhoeft <hi@xtian.us>
2019-07-09 18:25:33 -0600

    Update gems

--- db/migrate/20160608180004_add_faction_to_characters.rb
+++ db/migrate/20160608180004_add_faction_to_characters.rb
@@ -2,8 +2,6 @@
 
 class AddFactionToCharacters < ActiveRecord::Migration[5.0]
   def change
-    # rubocop:disable Rails/NotNullColumn
     add_column :characters, :faction, :integer, null: false
-    # rubocop:enable Rails/NotNullColumn
   end
 end
