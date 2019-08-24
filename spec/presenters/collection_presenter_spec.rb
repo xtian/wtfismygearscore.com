670f70d728f1e4af88c81055b2fb6211380cde91
Christian Wesselhoeft <hi@xtian.us>
2019-08-24 13:46:12 -0600

    Fix Rubocop errors

--- spec/presenters/collection_presenter_spec.rb
+++ spec/presenters/collection_presenter_spec.rb
@@ -4,11 +4,13 @@
 require 'rails_helper'
 
 RSpec.describe CollectionPresenter do
-  class TestPresenter
-    def initialize(object); end
+  let(:test_class) do
+    Class.new do
+      def initialize(object); end
 
-    def foo
-      'foo'
+      def foo
+        'foo'
+      end
     end
   end
 
@@ -17,18 +19,18 @@ RSpec.describe CollectionPresenter do
       relation = instance_double('ActiveRecord::Relation')
       expect(relation).to receive(:where)
 
-      described_class.new(relation, TestPresenter).where
+      described_class.new(relation, test_class).where
     end
 
     it 'returns an enumerable that wraps each item in a presenter' do
-      presented = described_class.new([double, double], TestPresenter)
+      presented = described_class.new([double, double], test_class)
 
       expect(presented.map(&:foo)).to eq(%w[foo foo])
     end
 
     it 'returns an enumerable that wraps calls to #to_ary' do
       relation = instance_double('ActiveRecord::Relation', to_ary: [double, double])
-      presented = described_class.new(relation, TestPresenter)
+      presented = described_class.new(relation, test_class)
 
       expect(presented.to_ary.map(&:foo)).to eq(%w[foo foo])
     end
