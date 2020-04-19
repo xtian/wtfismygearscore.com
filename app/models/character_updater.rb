b3b6dc664faf7c3f38a65e85378212b7e6f5e966
Christian Wesselhoeft <hi@xtian.us>
2020-04-19 13:14:34 -0600

    CharacterUpdater: Convert to module

--- app/models/character_updater.rb
+++ app/models/character_updater.rb
@@ -2,60 +2,44 @@
 # frozen_string_literal: true
 
 # Updates a given {Character} with latest data from the Armory
-class CharacterUpdater
-  # @param character [Character]
-  def initialize(character)
-    @character = character
-  end
+module CharacterUpdater
+  class << self
+    # Updates {Character} with latest Armory data unless it has been updated in
+    # the recent past. Will destroy {Character} if Armory returns a 404.
+    # @param character [Character]
+    # @return [Character] updated character
+    def call(character)
+      return character if recently_updated?(character)
 
-  # Convenience method to avoid object initialization
-  # @param character [Character]
-  # @return [Character]
-  def self.call(character)
-    new(character).call
-  end
+      armory_response = ARMORY.fetch_character(**params(character))
+      score = GearscoreCalculator.calculate(armory_response.items)
 
-  # Updates {Character} with latest Armory data unless it has been updated in
-  # the recent past. Will destroy {Character} if Armory returns a 404.
-  # @return [Character] updated character
-  def call
-    return character if recently_updated?
+      character.update_from_armory(armory_response, score)
+      character
+    rescue Armory::NotFoundError
+      handle_not_found(character)
+    end
 
-    character.update_from_armory(armory_response, score)
-    character
-  rescue Armory::NotFoundError
-    handle_not_found
-  end
+    private
 
-  private
+    def handle_not_found(character)
+      raise if character.new_record?
 
-  attr_reader :character
+      Rails.logger.warn "#{params(character)} did not resolve to a valid Armory profile. Deleting cached character."
+      character.destroy!
+    end
 
-  def armory_response
-    @_armory_response ||= ARMORY.fetch_character(**params)
-  end
+    def params(character)
+      {
+        region: character.region,
+        realm: character.realm,
+        name: character.name,
+      }
+    end
 
-  def handle_not_found
-    raise if character.new_record?
-
-    Rails.logger.warn "#{params} did not resolve to a valid Armory profile. Deleting cached character."
-    character.destroy!
-  end
-
-  def params
-    {
-      region: character.region,
-      realm: character.realm,
-      name: character.name,
-    }
-  end
-
-  def recently_updated?
-    # Don't update cache if it was updated less than fifteen minutes ago
-    !character.new_record? && character.updated_at > 15.minutes.ago
-  end
-
-  def score
-    GearscoreCalculator.calculate(armory_response.items)
+    def recently_updated?(character)
+      # Don't update cache if it was updated less than fifteen minutes ago
+      !character.new_record? && character.updated_at > 15.minutes.ago
+    end
   end
 end
