98117044f06c310cae101bde0db7445d22a87b55
Christian Wesselhoeft <hi@xtian.us>
2020-04-19 13:14:19 -0600

    CharacterUpdateBroadcaster: Convert to module

--- app/models/character_update_broadcaster.rb
+++ app/models/character_update_broadcaster.rb
@@ -2,43 +2,21 @@
 # frozen_string_literal: true
 
 # Encapsulates logic for broadcasting HTML character updates over ActionCable
-class CharacterUpdateBroadcaster
-  # @param character [Character]
-  # @param updated_at [String, DateTime]
-  def initialize(character, updated_at)
-    @caller_updated_at = updated_at
-    @character = character
-  end
-
-  # Convenience method to avoid object initialization
+module CharacterUpdateBroadcaster
+  # Broadcasts update to the relevant {CharacterUpdateChannel} if given
+  # {Character} was updated more recently than given timestamp
   # @param character [Character]
   # @param updated_at [String, DateTime]
   # @return [void]
   def self.call(character, updated_at)
-    new(character, updated_at).call
-  end
+    return unless character.updated_at > updated_at
 
-  # Broadcasts update to the relevant {CharacterUpdateChannel} if given
-  # {Character} was updated more recently than given timestamp
-  # @return [void]
-  def call
-    return unless character.updated_at > caller_updated_at
-
-    ActionCable.server.broadcast channel_name, html: partial, id: character.to_param
-  end
-
-  private
-
-  attr_reader :caller_updated_at, :character
-
-  def channel_name
-    "characters:#{character.to_param}:armory_updates"
-  end
-
-  def partial
-    CharactersController.render(
+    channel_name = "characters:#{character.to_param}:armory_updates"
+    partial = CharactersController.render(
       partial: "characters/character",
       locals: { character: CharacterPresenter.new(character) },
     )
+
+    ActionCable.server.broadcast channel_name, html: partial, id: character.to_param
   end
 end
